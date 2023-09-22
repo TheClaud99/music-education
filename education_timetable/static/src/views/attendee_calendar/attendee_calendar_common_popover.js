a91a76aa85501d24821feafcae032b90869f4098
theclaud99 <claudiomano@live.it>
2023-09-23 00:20:12 +0200

    Possibilità di eliminare eventi in attendee calendar

---
+++ education_timetable/static/src/views/attendee_calendar/attendee_calendar_common_popover.js
@@ -0,0 +1,14 @@
+/** @odoo-module **/
+
+import { AttendeeCalendarCommonPopover } from "@calendar/views/attendee_calendar/common/attendee_calendar_common_popover";
+import { patch } from "@web/core/utils/patch";
+
+patch(
+    AttendeeCalendarCommonPopover.prototype,
+    "attendee_calendar_model_everybody_by_default_patch",
+    {
+        get isEventArchivable() {
+            return true;
+        },
+    }
+);
