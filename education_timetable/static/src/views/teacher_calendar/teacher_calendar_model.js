77190e0b808d8baa246c9a511c09361ad03167c4
theclaud99 <claudiomano@live.it>
2023-10-02 17:07:49 +0200

    Rimosso filtro Everybody's calendar di default per insegnanti

--- education_timetable/static/src/views/teacher_calendar/teacher_calendar_model.js
+++ education_timetable/static/src/views/teacher_calendar/teacher_calendar_model.js
@@ -1,20 +1,29 @@
 /** @odoo-module **/
 
 import { CalendarModel } from "@web/views/calendar/calendar_model";
-
+import session from "web.session";
 
 export class TeacherCalendarModel extends CalendarModel {
     /**
      * @protected
      */
     async loadFilterSection(fieldName, filterInfo, previousSection) {
-        const previousFilters = previousSection
-            ? previousSection.filters
-            : [];
-        if (previousFilters.length != 0) {
-            return super.loadFilterSection(fieldName, filterInfo, previousSection);
+        const previousFilters = previousSection ? previousSection.filters : [];
+        const is_teacher = await session.user_has_group(
+            "education.education_teacher"
+        );
+        if (previousFilters.length != 0 || is_teacher) {
+            return super.loadFilterSection(
+                fieldName,
+                filterInfo,
+                previousSection
+            );
         }
-        const ret = await super.loadFilterSection(fieldName, filterInfo, previousSection);
+        const ret = await super.loadFilterSection(
+            fieldName,
+            filterInfo,
+            previousSection
+        );
         const filters = ret.filters;
         const previousActiveFilter = filters.find((f) => f.active);
         const allFilter = filters.find((f) => f.type === "all");
@@ -25,4 +34,3 @@ export class TeacherCalendarModel extends CalendarModel {
         return ret;
     }
 }
-
