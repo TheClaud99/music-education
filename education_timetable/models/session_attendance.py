d25211f81b978f78cf7812ba7f97b0b84f40fd57
c.mano <c.mano@progresso.srl>
2024-02-14 10:01:14 +0100

    Visualizzato timestamp inizio in elenco presenze

--- education_timetable/models/session_attendance.py
+++ education_timetable/models/session_attendance.py
@@ -1,9 +1,17 @@
-from odoo import fields, models
+from odoo import _, api, fields, models
 
 
 class EducationSessionAttendance(models.Model):
     _name = "education.session.attendance"
     _inherit = ["mail.thread"]
+
+    name = fields.Char(
+        "Name",
+        compute="_compute_name",
+        store=False,
+        readonly=True,
+        copy=False,
+    )
 
     session_id = fields.Many2one(
         comodel_name="education.session",
@@ -13,13 +21,22 @@ class EducationSessionAttendance(models.Model):
         ondelete="cascade",
     )
 
-    student_id = fields.Many2one(comodel_name="res.partner", string="Studente")
+    student_id = fields.Many2one("res.partner", "Studente", required=True)
     is_paid = fields.Boolean("Is Paid", copy=False, tracking=True)
 
     notes = fields.Char(string="Note")
 
     supporting_document = fields.Boolean(string="Supporting Document")
 
+    start = fields.Datetime("Start time", related="session_id.start")
+
     def set_paid(self):
         self.write({"is_paid": True})
         return {}
+
+    @api.depends("student_id", "session_id.start")
+    def _compute_name(self):
+        for attendance in self:
+            attendance.name = _(
+                "{} - {}".format(attendance.start, attendance.student_id.name)
+            )
